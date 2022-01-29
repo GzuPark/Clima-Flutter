224e8b5814f01b7d5e53799905cb11b490dea211
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-29 19:33:31 +0900

    Refactor to fetch weather data action with location button

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,27 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final String apiKey = dotenv.env['API_KEY'].toString();
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentPosition();
+
+    String url = '$openWeatherMapURL'
+        '?lat=${location.latitude}'
+        '&lon=${location.longitude}'
+        '&appid=$apiKey'
+        '&units=metric';
+
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
